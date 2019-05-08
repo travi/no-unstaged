0f633d9fae289450b29c6f4bd378d37d28305082
Matt Travi <programmer@travi.org>
2019-05-08 01:40:03 -0500

    build(rollup): added the babel plugin for transpiling jsx

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,19 @@
 /* eslint import/no-extraneous-dependencies: ['error', {'devDependencies': true}] */
 import executable from 'rollup-plugin-executable';
 import autoExternal from 'rollup-plugin-auto-external';
+import babel from 'rollup-plugin-babel';
 
 export default {
   input: 'src/index.js',
   plugins: [
     executable(),
-    autoExternal()
+    autoExternal(),
+    babel({
+      babelrc: false,
+      externalHelpers: true,
+      exclude: ['./node_modules/**'],
+      presets: [['@travi', {targets: {node: 8}, react: true}]]
+    })
   ],
   output: [{file: 'bin/no-unstaged.js', format: 'cjs', sourcemap: true, banner: '#!/usr/bin/env node'}]
 };
